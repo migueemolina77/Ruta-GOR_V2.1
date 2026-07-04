bda949b6781b86ec1199f8c6b3521d92cdf53350
migueemolina77 <miguelmolinave@gmail.com>
2026-07-04 14:57:11 -0500

    Update App.py

--- App.py
+++ App.py
@@ -70,7 +70,7 @@ PUNTOS_CRITICOS_VALIDACION = {
     # ==================================================
     # COMUNIDADES
     # ==================================================
-    "AEROPUERTO _MORELIA = {
+    "AEROPUERTO _MORELIA": {
         "lat": 3.750656,
         "lon": -71.455936,
         "radio_critico_km": 2.0,
